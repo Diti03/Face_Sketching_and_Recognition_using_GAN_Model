4b49ac0ea4b3f7805c78d20a322b8dd665557ac2
Arya-Nigade <94315959+Arya-Nigade@users.noreply.github.com>
2025-07-02 02:36:47 +0530

    Update supabase.client.ts

--- WitnessArt/src/app/supabase.client.ts
+++ WitnessArt/src/app/supabase.client.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gnvbihjdfpguputahzyz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdudmJpaGpkZnBndXB1dGFoenl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3OTg0OTYsImV4cCI6MjA2MjM3NDQ5Nn0.92MpN58SwSFHZlwWf66ZcAZ-_TWkJEJGj06zwj5Pt5M';  // from Supabase → Project Settings → API
-
+const supabaseUrl = "YOUR_SUPABASEURL";
+const supabaseKey = "YOUR_SUPABASEKEY";
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
